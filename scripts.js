3cb5377ab80800337044336b384c72e33dd69126
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2024-09-05 10:47:09 +0200

    órai

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const honapok = {0:"január", 1:"február", 2:"március", 3:"április", 4:"május",
+     5: "június", 6:"július", 7:"augusztus", 8:"szeptember", 9:"október", 10:"november", 11:"december"}
+
+let FeltoltHonapok = () => {
+    for(let i = 0; i < 12; i++){
+
+    }
+}
